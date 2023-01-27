375b736eb4a52b4cf3fb36f8a7a73fa84f4b7eeb
Nick-Stoyanov <99138648+Nick-Stoyanov@users.noreply.github.com>
2023-01-26 19:35:38 -0500

    Initial setup. Library (JAR) works

---
+++ src/main/java/test/test.java
@@ -0,0 +1,12 @@
+package test;
+
+import edu.ccri.lesson02.assignment.dashboard.Automobile;
+
+public class test
+{
+
+    public static void main(String[]args){
+        Automobile car = new Automobile("car");
+        System.out.println(car.toString());
+    }
+}
